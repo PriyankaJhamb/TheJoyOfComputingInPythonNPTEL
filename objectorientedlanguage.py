fe1889c1f30c5fa3e80d08b61e8313e289232bc0
PriyankaJhamb <jhambpriyanka73@gmail.com>
2021-06-25 15:48:27 +0530

    commited

--- objectorientedlanguage.py
+++ objectorientedlanguage.py
@@ -1,22 +1,48 @@
 class Employee:
     increment = 1.5
+    no_of_employees=0
+
     def __init__(self, fname, lname, salary):
         self.fname = fname
         self.lname = lname
         self.salary = salary
+        Employee.no_of_employees+=1
+
     def increase(self):
         self.salary=self.salary * Employee.increment
 
-harry = Employee("harry","Singh", 440000)
-rohan = Employee("rohan", "das" , 450000)
+    @classmethod
+    def change_increment(cls, amount):
+        cls.increment=amount
 
-print(harry.fname, rohan.lname)
-print(harry.salary)
-harry.increase();
-print(harry.salary)
-print(Employee.__dict__)
-print(harry.__dict__)
-print(rohan.__dict__)
+    @classmethod #class method as an alternative constructor
+    def from_str(cls, emp_string):
+        fname, lname, salary = emp_string.split("-")
+        return cls(fname,lname,salary)
+
+    @staticmethod
+    def isopen(day):
+        if day=="SUNDAY":
+            return False
+        else:
+            return True
+
+# print(Employee.isopen("MONDAY"))
+# Priyanka=Employee.from_str("Priyanka-Jhamb-65900")
+# print(Priyanka.fname,"\n",Priyanka.lname,"\n",Priyanka.salary)
+# print(Employee.no_of_employees)
+# harry = Employee("harry","Singh", 440000)
+# print(Employee.no_of_employees)
+# rohan = Employee("rohan", "das" , 450000)
+# print(harry.no_of_employees)
+# print(harry.fname, rohan.lname)
+# print(harry.salary)
+# Employee.change_increment(4)
+# harry.increase();
+# print(harry.salary)
+# print(Employee.__dict__)
+# print(harry.__dict__)
+# print(rohan.__dict__)
 
 
 
