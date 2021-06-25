d1c56c9a402aacf72dbfb297e838ffc1e88a623c
PriyankaJhamb <jhambpriyanka73@gmail.com>
2021-06-25 17:03:34 +0530

    Object oriented language concepts revision

--- objectorientedlanguage.py
+++ objectorientedlanguage.py
@@ -8,6 +8,25 @@ class Employee:
         self.salary = salary
         Employee.no_of_employees+=1
 
+    @property
+    def email(self):
+        if self.fname==None and self.lname==None:
+            return 'Email not set'
+        else:
+            return self.fname +'.'+ self.lname +'@gmail.com'
+
+    @email.setter
+    def email(self, given_email):
+        name_list = given_email.split("@")[0].split(".")
+        self.fname=name_list[0]
+        self.lname=name_list[1]
+
+    @email.deleter
+    def email(self):
+        self.fname=None
+        self.lname=None
+
+
     def increase(self):
         self.salary=self.salary * Employee.increment
         # return self.salary
@@ -27,6 +46,8 @@ class Employee:
             return False
         else:
             return True
+
+
     # Magic Functions (Dundar Methods)
 
     def __add__(self, other):
@@ -38,13 +59,22 @@ class Employee:
         return "The name of the Employee is {}".format(self.fname)
 
 sonu=Employee("SOnu","Kakkar", 234235454)
-Neha=Employee("Neha","Kakkar", 23343235454)
-a=4
-print(a.__add__(34))
-print(sonu+Neha)
-print(repr(sonu))
-print(str(sonu))
-print(Neha)
+print(sonu.email)
+sonu.lname="Kaur"
+print(sonu.email)
+sonu.email="kakkar.sonu@gmail.com"
+print(sonu.email)
+del sonu.email
+print(sonu.email)
+# Neha=Employee("Neha","Kakkar", 23343235454)
+# a=4
+# print(a.__add__(34))
+# print(sonu+Neha)
+# print(repr(sonu))
+# print(str(sonu))
+# print(Neha)
+
+
 #inheritance
 
 class Programmer(Employee):
